1738a834f7ac5be33da1cc61e8ffc70e607972d6
Antarctic-penguin <jamesantarcticpenguin@gmail.com>
2023-02-19 20:59:30 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -38,22 +38,46 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error)) // 錯誤處理
 })
 
+// 新增資料的頁面
 app.get('/todos/new', (req, res) => {
-  return res.render('new')
+  res.render('new')
 })
-
+// 使用者新增資料後處理路由
 app.post('/todos', (req, res) => {
   const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name })     // 存入資料庫
+  Todo.create({ name })     // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
 })
 
+// 顯示詳細資料的頁面
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
-  return Todo.findById(id)
+  Todo.findById(id)
     .lean()
     .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
+// 修改資料的頁面
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+// 使用者修改資料後的路由
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
