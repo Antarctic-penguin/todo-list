d6a7db6e907a1bf0ab685bcb483970ef70988b21
Antarctic-penguin <jamesantarcticpenguin@gmail.com>
2023-07-11 11:48:17 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,8 +2,18 @@
 const express = require('express')
 const router = express.Router()
 
+// 登錄頁路由
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+// 登錄路由
+router.post('/login', (req, res) => {
+})
+
+// 註冊頁路由
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 module.exports = router
