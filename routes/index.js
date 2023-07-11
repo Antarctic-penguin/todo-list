579b653d7e1f3a50b63e99d6e17a940c4457ecd8
Antarctic-penguin <jamesantarcticpenguin@gmail.com>
2023-07-11 10:09:06 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,10 @@ const router = express.Router()
 // 引入 home 模組程式碼
 const home = require('./modules/home')
 const todo = require('./modules/todo')
+const users = require('./modules/users')
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 router.use('/todos', todo)
+router.use('/users', users)
 // 匯出路由器
 module.exports = router
