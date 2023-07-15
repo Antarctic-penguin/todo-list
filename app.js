caef33121d0e9e733db12be766a62593d98be458
Antarctic-penguin <jamesantarcticpenguin@gmail.com>
2023-07-15 21:48:28 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')   // 引用套件
 // 載入passport設定檔，要寫在 express-session 以後
 const usePassport = require('./config/passport')
 
@@ -27,11 +28,16 @@ app.use(session({
 usePassport(app)
 
 // 用app.use 代表這組 middleware 會作用於所有的路由
+app.use(flash())  // 掛載套件
 app.use((req, res, next) => {
   // 把 req.isAuthenticated() 回傳的布林值，交接給 res 使用
   res.locals.isAuthenticated = req.isAuthenticated()
   // 把使用者資料交接給 res 使用
   res.locals.user = req.user
+  // 設定 success_msg 訊息
+  res.locals.success_msg = req.flash('success_msg')
+  // 設定 warning_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
